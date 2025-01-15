f9ba6c0db3c681a5fe148740ce89f97346c29058
kevintian4 <kevintian20@gmail.com>
2025-01-15 13:08:05 -0800

    replaced cookie approach with api

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import time
-import pickle
+import os
+import random
+from twocaptcha import TwoCaptcha
 from selenium import webdriver
-from selenium.webdriver.common.by import By  # Import By for using different locators
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file and retrieve credentials
+load_dotenv()
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
 # Store URL of activity
 activityUrl = "https://anc.ca.apm.activecommunities.com/burnaby/activity/search/detail/50889?onlineSiteId=0&from_original_cui=true"
@@ -11,21 +23,27 @@ driver = webdriver.Chrome()
 loginPageUrl = "https://anc.ca.apm.activecommunities.com/burnaby/signin"
 driver.get(loginPageUrl)
 
-# Load cookies from the saved file
-with open("cookies.pkl", "rb") as cookie_file:
-    cookies = pickle.load(cookie_file)
-    for cookie in cookies:
-        driver.add_cookie(cookie)
+# Enter the email
+email_input = driver.find_element(By.XPATH, '//input[@placeholder="Enter your Email address"]')
+email_input.send_keys(email)
 
-# Refresh the page to apply the cookies and be logged in
-driver.refresh()
+# Enter the password
+password_input = driver.find_element(By.XPATH, '//input[@type="password"]')
+password_input.send_keys(password)
 
-# Navigate to the activity page
-driver.get(activityUrl)
+api_key = os.getenv("CAPTCHA_API_KEY")
+sitekey = "6LdJqskaAAAAAFsn-RktxwmkRKhBEoilSTX89roU"
+solver = TwoCaptcha(api_key)
+result = solver.recaptcha(
+    sitekey=sitekey,
+    url=loginPageUrl
+)
 
-# Use By.XPATH to locate the button and click it
-submit_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
-submit_button.click()
+print(f'Result: {result["code"]}')
+
+# # Submit the form
+# submit_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
+# submit_button.click()
 
 # Don't close the browser immediately
 time.sleep(300)
